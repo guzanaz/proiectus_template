d6ceaf1d19fb899cb1d2c8e23ee61221f14533ae
Daniela <daniela.gallardo.reyes@gmail.com>
2019-12-11 19:34:18 +0100

    update
    @cpopescuakx

---
+++ php/form.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Footer Proiectus</title>
+
+    <!-- css bootstrap included -->
+    <link rel="stylesheet" href="../css/main.css">
+    
+    <!-- googlefonts Montserrat y Roboto -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Roboto:300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <form>
+        <!-- inicio formulario -->
+        <div class="row justify-content-center">
+
+            <!-- grid container formulario-->
+            <div class="col-11 col-sm-11 col-md-10 col-lg-10 col-xl-10">
+
+                <!-- container formulario -->
+                <div class="container contact-form">
+                    <!-- Imagen formulario -->
+                    <div class="contact-image">
+                        <img src="../img/logos_proiectus/icono_negro.png" alt="proiectus_forms"/>
+                    </div>
+                    
+                    <div class="container">
+                        <div class="row no-gutters justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                <h1>Proiectus forms.</h1>
+                                <p>Estás en la plantilla proiectus forms, toma lo que sea útil para tu formulario, aplícalo bien, haz tu trabajo.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Input básico -->
+                    <div class="form-group mt-4">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                <label for="exampleFormControlInput1">Tipo de información</label>
+                                <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Ejemplo del tipo de información">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin input básico -->
+
+
+                    <!-- select básico -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                <label for="exampleFormControlSelect1">Ejemplo de select</label>
+                                <select class="form-control" id="exampleFormControlSelect1">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Fin select básico -->
+
+                    <!-- select múltiple -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                <label for="exampleFormControlSelect2">Ejemplo de select múltiple</label>
+                                <select multiple class="form-control" id="exampleFormControlSelect2">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Fin select múltiple -->
+
+                    <!-- text área -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                <label for="exampleFormControlTextarea1">Ejemplo de textarea</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Ejemplo del tipo de información"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin text área -->
+
+                    <!-- inicio checkbox -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                                    <label class="form-check-label" for="gridCheck">Ejemplo de checkbox</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin checkbox-->
+                    
+
+                    <!-- radio button -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                <div class="custom-control custom-radio">
+                                    <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadio1"> un radio button </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin radio button -->
+
+                    <!-- radio button 2 -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                <div class="custom-control custom-radio">
+                                    <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadio2">otro radio button</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin radio button 2-->
+
+                    <!-- radio button 3 -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                <div class="custom-control custom-radio">
+                                    <input type="radio" id="customRadio3" name="customRadio" class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadio3"> Y otro radio button!</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin radio button 3-->
+
+                    <!-- elemento -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10  col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin elemento -->
+
+                    <!-- elemento -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin elemento -->
+
+                    
+                
+                </div> 
+                <!-- fin container formulario -->
+
+            </div>
+            <!--  fin grid container formulario-->
+
+        </div>
+        <!-- fin formulario -->
+    </form>
+
+</body>
+</html>
