5ded9cfc75f0a223409562ba7e7ae35c9072bcde
Daniela <daniela.gallardo.reyes@gmail.com>
2019-12-12 09:28:09 +0100

    update
    modificaciones del 11/12/19

--- php/form.php
+++ php/form.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Roboto:300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
 
 </head>
-<body>
+<body id="body">
     <form>
         <!-- inicio formulario -->
         <div class="row justify-content-center">
@@ -21,15 +21,23 @@
             <!-- grid container formulario-->
             <div class="col-11 col-sm-11 col-md-10 col-lg-10 col-xl-10">
 
+                    <!-- Imagen formulario -->
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+                                <div class="contact-image">
+                                    <img src="../img/logos_proiectus/icono_negro.png" alt="proiectus_forms"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 <!-- container formulario -->
                 <div class="container contact-form">
-                    <!-- Imagen formulario -->
-                    <div class="contact-image">
-                        <img src="../img/logos_proiectus/icono_negro.png" alt="proiectus_forms"/>
-                    </div>
+
                     
                     <div class="container">
-                        <div class="row no-gutters justify-content-center">
+                        <div class="row no-gutters justify-content-center mt-5">
                             <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
                                 <h1>Proiectus forms.</h1>
                                 <p>Estás en la plantilla proiectus forms, toma lo que sea útil para tu formulario, aplícalo bien, haz tu trabajo.</p>
@@ -151,6 +159,78 @@
                     <div class="form-group">
                         <div class="row justify-content-center">
                             <div class="col-10  col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                <h3>Custom File</h3>
+                                <p>To create a custom file upload, wrap a container element with a class of .custom-file around the input with type="file". Then add the .custom-file-input to the file input:</p>
+                                <form action="/action_page.php">
+                                    <p>Custom file:</p>
+                                    <div class="custom-file mb-3">
+                                    <input type="file" class="custom-file-input" id="customFile" name="filename">
+                                    <label class="custom-file-label" for="customFile">Choose file</label>
+                                    </div>
+                                    </div>
+                        </div>
+                    </div>
+                            
+                    <script>
+                    // Add the following code if you want the name of the file appear on select
+                    $(".custom-file-input").on("change", function() {
+                    var fileName = $(this).val().split("\\").pop();
+                    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+                    });
+                    </script>
+
+            
+                    <!-- fin elemento -->
+
+                    <!-- elemento -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                
+
+
+                                <nav aria-label="...">
+                                <ul class="pagination">
+                                    <li class="page-item disabled">
+                                    <span class="page-link">Previous</span>
+                                    </li>
+                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item active">
+                                    <span class="page-link">
+                                        2
+                                        <span class="sr-only">(current)</span>
+                                    </span>
+                                    </li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item">
+                                    <a class="page-link" href="#">Next</a>
+                                    </li>
+                                </ul>
+                                </nav>
+
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin elemento -->
+
+
+                    <!-- elemento -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                    
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin elemento -->
+
+                    <!-- elemento -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
                                 
                             </div>
                         </div>
@@ -167,6 +247,17 @@
                     </div>
                     <!-- fin elemento -->
 
+
+                    <!-- elemento -->
+                    <div class="form-group">
+                        <div class="row justify-content-center">
+                            <div class="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-6">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <!-- fin elemento -->
+                    
                     
                 
                 </div> 
